409253ded209269e12737bdf59f79f3db7221a68
KivutiBrian <briankivuti1@gmail.com>
2021-03-08 13:16:02 +0300

    api setup

---
+++ horesy-backend-api/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    title='Horesy API',
+    version='0.0.1',
+    description='services and endpoints for Horesy',
+    redoc_url='/'
+)
+
+# setup the origins
+origins = ["http://localhost","http://localhost:3000","http://127.0.0.1", ]
+
+# add the middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
